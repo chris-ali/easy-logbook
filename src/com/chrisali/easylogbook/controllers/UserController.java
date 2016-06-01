47ec2b26fd765a03fc9f4223860bc234ce0f4d38
Christopher Ali <christopher.ali@outlook.com>
2016-06-01 14:25:18 -0400

    Thymeleaf Dependency, Profile Page, Delete User, Logout Bug
    - Added missing core thymeleaf dependency
    
    - Fixed incorrect logout urls in security-context.xml causing user
    unable to log out
    
    - Added logic to delete all items associated with a user upon deletion
    in UserController
    
    - Added javascript to check old password; need to get thymeleaf inline
    javascript working
    
    - Cleaned up header in logbookcreated.html

--- src/com/chrisali/easylogbook/controllers/UserController.java
+++ src/com/chrisali/easylogbook/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.chrisali.easylogbook.controllers;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +13,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.chrisali.easylogbook.beans.Aircraft;
+import com.chrisali.easylogbook.beans.Logbook;
+import com.chrisali.easylogbook.beans.LogbookEntry;
 import com.chrisali.easylogbook.beans.User;
+import com.chrisali.easylogbook.services.AircraftService;
+import com.chrisali.easylogbook.services.LogbookEntryService;
+import com.chrisali.easylogbook.services.LogbookService;
 import com.chrisali.easylogbook.services.UsersService;
 import com.chrisali.easylogbook.validation.FormValidationGroup;
 
@@ -20,6 +28,18 @@ public class UserController {
 	
 	@Autowired
 	private UsersService usersService;
+	
+	@Autowired
+	private LogbookService logbookService;
+	
+	@Autowired
+	private AircraftService aircraftService;
+	
+	@Autowired
+	private LogbookEntryService logbookEntryService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@RequestMapping("user/create")
 	public String showCreateAccount(Model model) {
@@ -58,8 +78,43 @@ public class UserController {
 		User user = usersService.getUser(principal.getName());
 		
 		model.addAttribute("user", user);
+		model.addAttribute("oldPassword", user.getPassword());
+		model.addAttribute("passwordEncoder", passwordEncoder);
 		
 		return "profile/profile";
+	}
+	
+	@RequestMapping("user/update")
+	public String doUpdateAccount(@Validated(FormValidationGroup.class) User user, BindingResult result, Model model) {
+		if (result.hasErrors())
+			return "profile/profile";
+		
+		usersService.createOrUpdate(user);
+		
+		return "profile/profile";
+	}
+
+	@RequestMapping("user/close")
+	public String doCloseAccount(Principal principal, BindingResult result, Model model) {
+		String username = principal.getName();
+		
+		List<Logbook> logbooks = logbookService.getLogbooks(username);
+		for (Logbook logbook : logbooks) {
+			List<LogbookEntry> entries = logbookEntryService.getLogbookEntries(logbook.getId());
+
+			for (LogbookEntry entry : entries)
+				logbookEntryService.delete(logbook.getId(), entry.getId());
+			
+			logbookService.delete(username, logbook.getId());
+		}
+		
+		List<Aircraft> aircraftList = aircraftService.getAircraft(username);
+		for (Aircraft aircraft : aircraftList) 
+			aircraftService.delete(username, aircraft.getId());
+		
+		usersService.delete(username);
+		
+		return "home/home";
 	}
 	
 	@RequestMapping("user/login")
